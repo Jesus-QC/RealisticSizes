333b6649686397352af71d0d68d0e2f416dd85db
Jesús <69375249+JesusQCHub@users.noreply.github.com>
2020-09-02 15:37:04 +0200

    Intercom bug fixed + better for RP

--- Config.cs
+++ Config.cs
@@ -1,9 +1,11 @@
 ﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
 
 namespace RealisticSizes
 {
     public sealed class Config : IConfig
     {
+        [Description("Is the plugin enabled?")]
         public bool IsEnabled { get; set; } = true;
     }
 }
