635e4c8f95cb7f7eb248042d7c98a276d1bb2599
jesus-qc <jesusquirocampos@gmail.com>
2021-03-04 15:45:52 +0100

    version bump

--- RealisticSizes/RealisticSizes.cs
+++ RealisticSizes/RealisticSizes.cs
@@ -12,7 +12,7 @@ namespace RealisticSizes
         public override string Author { get; } = "JesusQC";
         public override string Prefix { get; } = "RealisticSizes";
         public override Version RequiredExiledVersion { get; } = new Version(2, 3, 4);
-        public override Version Version { get; } = new Version(3, 1, 5);
+        public override Version Version { get; } = new Version(3, 1, 6);
 
         public override void OnEnabled()
         {
