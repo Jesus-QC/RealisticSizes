9e50bb865f677115bf524ec97ee312866c7b9693
4310V343k <denromanov2005@gmail.com>
2021-03-04 17:17:26 +0300

    exiled bump

--- RealisticSizes/RealisticSizes.cs
+++ RealisticSizes/RealisticSizes.cs
@@ -11,7 +11,7 @@ namespace RealisticSizes
         public override string Name { get; } = "RealisticSizes";
         public override string Author { get; } = "JesusQC";
         public override string Prefix { get; } = "RealisticSizes";
-        public override Version RequiredExiledVersion { get; } = new Version(2, 3, 4);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 6, 0);
         public override Version Version { get; } = new Version(3, 1, 5);
 
         public override void OnEnabled()
